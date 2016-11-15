b47affada56d028739bada2b47c3561e702bdc36
niemandkun <niemandkun@yandex.ru>
2016-11-15 20:17:15 +0500

    Add SparseSigmaMap to effectively store maps with huge amount of entities similar to each other

---
+++ HoMM/Generators/Maps/SparseSigmaMap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HoMM.Generators
+{
+    class SparseSigmaMap<TCell> : ImmutableSigmaMap<TCell>
+    {
+        public override MapSize Size { get; }
+
+        private IDictionary<SigmaIndex, TCell> cells;
+        private TCell defaultValue;
+
+        public SparseSigmaMap(MapSize size, IDictionary<SigmaIndex, TCell> cells, 
+            TCell defaultValue = default(TCell))
+        {
+            Size = size;
+            this.cells = cells;
+            this.defaultValue = defaultValue;
+        }
+
+        public SparseSigmaMap(MapSize size, Func<SigmaIndex, TCell> cellsFactory,
+            TCell defaultValue = default(TCell))
+            : this(size, new Dictionary<SigmaIndex, TCell>(), defaultValue)
+        {
+            foreach (var index in SigmaIndex.Square(size))
+            {
+                var cell = cellsFactory(index);
+                
+                if (!cell.Equals(defaultValue))
+                    cells.Add(index, cell);
+            }
+        }
+
+        public override TCell this[SigmaIndex location]
+        {
+            get { return cells.ContainsKey(location) ? cells[location] : defaultValue; }
+        }
+    }
+
+    static class SparseSigmaMap
+    {
+        public static SparseSigmaMap<TCell> From<TCell>(ISigmaMap<TCell> source, 
+            TCell defaultValue = default(TCell))
+        {
+            return new SparseSigmaMap<TCell>(source.Size, i => source[i]);
+        }
+    }
+}
