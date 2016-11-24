a68119a952b8976e0f724f3e08616c1035ff3fda
niemandkun <niemandkun@yandex.ru>
2016-11-24 20:41:51 +0500

    road generator refactoring

--- HoMM/Generators/Terrain/BfsRoadGenerator.cs
+++ HoMM/Generators/Terrain/BfsRoadGenerator.cs
@@ -17,13 +17,18 @@ namespace HoMM.Generators
         
         public ISigmaMap<TileTerrain> Construct(ISigmaMap<MazeCell> maze)
         {
-            var road = new HashSet<SigmaIndex>(Graph.BreadthFirstSearch(SigmaIndex.Zero, 
-                    s => s.Neighborhood.Where(n => n.IsInside(maze.Size) && maze[n] == MazeCell.Empty),
-                    s => s.X == maze.Size.X-1 && s.Y == maze.Size.Y-1))
-                .Select(x => x.AboveDiagonal(maze.Size));
+            var road = FindRoad(SigmaIndex.Zero, new SigmaIndex(maze.Size.X - 1, maze.Size.Y - 1), maze);
             
             return new ArraySigmaMap<TileTerrain>(maze.Size, 
                 i => road.Contains(i.AboveDiagonal(maze.Size)) ? roadTile : null);
+        }
+
+        private HashSet<SigmaIndex> FindRoad(SigmaIndex from, SigmaIndex to, ISigmaMap<MazeCell> maze)
+        {
+            return new HashSet<SigmaIndex>(Graph.BreadthFirstSearch(from,
+                    s => s.Neighborhood.Where(n => n.IsInside(maze.Size) && maze[n] == MazeCell.Empty),
+                    s => s == to)
+                .Select(x => x.AboveDiagonal(maze.Size)));
         }
     }
 }
