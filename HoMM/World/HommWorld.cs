fb2322a9ad12fc152ca9397d12d93500888f1659
niemandkun <niemandkun@yandex.ru>
2016-12-01 12:40:41 +0500

    + engine

--- HoMM/World/HommWorld.cs
+++ HoMM/World/HommWorld.cs
@@ -6,10 +6,16 @@ using CVARC.V2;
 
 namespace HoMM.World
 {
-    class HommWorld : World<HommWorldState>
+    sealed class HommWorld : World<HommWorldState>
     {
+        public IHommEngine HommEngine { get; private set; }
+        public ICommonEngine CommonEngine { get; private set; }
+
         public override void CreateWorld()
         {
+            HommEngine = GetEngine<IHommEngine>();
+            CommonEngine = GetEngine<ICommonEngine>();
+
             throw new NotImplementedException();
         }
     }
---
+++ HoMM/World/IHommEngine.cs
@@ -0,0 +1,8 @@
+﻿using CVARC.V2;
+
+namespace HoMM.World
+{
+    interface IHommEngine : IEngine
+    {
+    }
+}
